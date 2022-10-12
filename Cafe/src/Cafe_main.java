238b60243099d0d40b0ef591607767d4d95c797b
User <User@DESKTOP-FMNN920>
2022-10-12 17:29:23 +0900

    Cafe

---
+++ Cafe/src/Cafe_main.java
@@ -0,0 +1,30 @@
+
+public class Cafe_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		
+		
+		//메뉴이름을 읽기
+		//메뉴이름이 "x"가 아닌 동안 (A)
+		//메뉴이름을 alMenu 에 저장
+		//메뉴가격을 읽기
+		//메뉴가격을 alPrice 에 저장
+		//메뉴이름을 읽기 -> A로 이동/반복
+		
+		//메뉴이름/가격을 출력
+		//Americano, 2500
+		//Latte, 3000
+		//Espresso, 2700
+		
+		Menu menu = new Menu();
+	
+//		menu.setAlMenu();
+	
+		
+		
+	}
+
+}
---
+++ Cafe/src/Menu.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Menu {
+	private ArrayList alMenu;
+	private ArrayList alPrice;
+	
+	public ArrayList getAlMenu() {
+		return alMenu;
+	}
+	
+	public void setAlMenu(ArrayList alMenu) {
+		this.alMenu = alMenu;
+	}
+	
+	public ArrayList getAlPrice() {
+		return alPrice;
+	}
+	
+	public void setAlPrice(ArrayList alPrice) {
+		this.alPrice = alPrice;
+	}
+	
+	
+	
+
+	
+	
+}
